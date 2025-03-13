34f7770261d166c7e962a0d7cf52e31a9c0a8c21
Windings-Lab <janton20001@gmail.com>
2025-03-13 14:24:05 +0200

    Anton Zhukov - Solution

--- task.sql
+++ task.sql
@@ -1,2 +1,7 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE INDEX idx_product_name ON Products (Name);
+CREATE INDEX idx_customer_email ON Customers (Email);
+
+ALTER TABLE Products RENAME INDEX idx_product_name TO Name;
+ALTER TABLE Customers RENAME INDEX idx_customer_email TO Email;
