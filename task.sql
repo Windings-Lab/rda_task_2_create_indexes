05fff2f2f09fdc41ba5229d2e4258489e0bf35f9
Windings-Lab <janton20001@gmail.com>
2025-03-13 14:26:35 +0200

    AI FIXES. One of the problem

--- task.sql
+++ task.sql
@@ -1,7 +1,4 @@
 USE ShopDB;
 
-CREATE INDEX idx_product_name ON Products (Name);
-CREATE INDEX idx_customer_email ON Customers (Email);
-
-ALTER TABLE Products RENAME INDEX idx_product_name TO Name;
-ALTER TABLE Customers RENAME INDEX idx_customer_email TO Email;
+CREATE INDEX Name ON Products (Name);
+CREATE INDEX Email ON Customers (Email);
